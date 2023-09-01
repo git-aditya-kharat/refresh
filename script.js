c113dea79ac501d79ed459cdd5aab4caf37c2c41
git-aditya-kharat <143285957+git-aditya-kharat@users.noreply.github.com>
2023-09-01 13:50:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeButton').addEventListener('click', function() {
+    const displayTextElement = document.getElementById('displayText');
+    displayTextElement.textContent = 'Text has been changed!';
+});
